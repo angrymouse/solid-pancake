dd189fd63154308c0efc1d37c3c5339e6043309e
Nikita Rykov <40735471+miceve@users.noreply.github.com>
2019-11-02 02:03:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let server=new net.Server();
 const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 server.on("connection",socket=>{
-let redisCli=net.connect(process.env.RURL)
+let redisCli=net.connect(process.env.RPORT,process.env.RHOST)
 socket.pipe(redisCli);
 redisCli.pipe(socket)
 })
